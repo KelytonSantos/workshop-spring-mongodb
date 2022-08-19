cfa500ed7959c126484252e7c0769d87ac3c776a
lucas santos <kelytonlucas@gmail.com>
2022-08-18 23:10:38 -0300

    Atualização de usuario com put

--- src/main/java/com/lucasantos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lucasantos/workshopmongo/resources/UserResource.java
@@ -9,11 +9,13 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 // import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -63,4 +65,12 @@ public class UserResource {
         userService.delete(id);
         return ResponseEntity.noContent().build();
      }
+
+     @PutMapping(value = "/{id}")
+     public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+        User obj = userService.fromDTO(objDto);//
+        obj.setId(id);
+        obj = userService.update(obj);//atualizando
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/lucasantos/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasantos/workshopmongo/services/UserService.java
@@ -30,16 +30,33 @@ public class UserService {
         return user;
     }
 
+    public User insert(User obj){        
+        return repo.insert(obj);
+    }
+
     public void delete(String id){
         findById(id);
         repo.deleteById(id);
     }
 
-    public User insert(User obj){        
-        return repo.insert(obj);
+	public User update(User obj) {
+		User newObj = findById(obj.getId()).orElse(new User());//objeto original do banco
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+    public void updateData(User newObj, User obj){
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+     public void updateData(User newObj, UserDTO obj){
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
     }
 
     public User fromDTO(UserDTO objDto){
-        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+        User user = new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+        return user;
     }
 }
